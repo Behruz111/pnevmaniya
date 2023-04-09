b0b139decf992ea15330560959b9a64e37a49a9d
Behruz <you@example.com>
2023-04-09 14:14:35 +0500

    first commit

---
+++ pnevmanya.py
@@ -0,0 +1,27 @@
+import streamlit as st
+from fastai.vision.all import *
+import pathlib
+temp = pathlib.PosixPath
+pathlib.PosixPath = pathlib.WindowsPath
+import plotly.express as px
+
+
+st.title("Pnevmanya kassalligini aniqlash")
+
+#rasm yuklash
+file = st.file_uploader("Rengen natijasini yuklang", type=['jpeg','png','gif','svg','jpg'])
+
+if file:
+    st.image(file)
+    #PIL CONVERT
+    img = PILImage.create(file)
+    model = load_learner('Pnevmanya kllasifikatsiya.pkl')
+    
+    #predection
+    pred, pred_id, probs = model.predict(img)
+    st.success(f"Bashorat: {pred}")
+    st.info(f"Ehtimolligi: {probs[pred_id]*100:.1f}%")
+
+    #lotting
+    fig = px.bar(y=probs*100, x=model.dls.vocab)
+    st.plotly_chart(fig)
